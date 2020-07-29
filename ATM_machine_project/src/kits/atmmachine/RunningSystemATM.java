9d33b6a1927e78305ea36fa7f368d30ac9aa8038
PC09 <phungdinhhuy95@gmail.com>
2020-07-29 10:22:40 +0700

    Done cashdispenser

--- ATM_machine_project/src/kits/atmmachine/RunningSystemATM.java
+++
@@ -1,9 +0,0 @@
-package kits.atmmachine;
-
-public class RunningSystemATM {
-public static void main(String[] args) {
-	ManagerATM managerATM=new ManagerATM();
-	managerATM.runningSystem();
-	
-}
-}
